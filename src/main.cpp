c675dfd8fd8404bcc0b054189f149bf096e7324f
LUCAS RUFATO TODESCO <39990609802@info.local>
2026-04-15 16:22:31 -0300

    Implementada as funções para a conexão WiFi

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,78 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
 
-// put function declarations here:
-int myFunction(int, int);
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+const char* WIFI_SSID = "SALA 09";
+const char* WIFI_SENHA = "info@134";
+
+void conectarWifi();
+void garantirWifiConectado();
+
+
+void setup()
+{
+  Serial.begin(9600);
+  conectarWifi();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+   garantirWifiConectado();
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void conectarWifi()
+{
+  Serial.println("================================");
+  Serial.println("Iniciando conexão WiFi...");
+  Serial.println("================================");
+
+  //Configura o ESP32 como station, ou seja
+  //ele vai se conectar a roteador existente
+  WiFi.mode(WIFI_STA); 
+
+  //Inicia a conexão com SSID e senha 
+  WiFi.begin(WIFI_SSID, WIFI_SENHA);
+
+  Serial.println("Conectando");
+
+  int tentativasWiFI = 0;
+  const int maxTentatviasWiFi = 30;
+
+  //Aguarda a conexão por até 30 tentativas
+  while(WiFi.status() != WL_CONNECTED && tentativasWiFI < maxTentatviasWiFi);
+  {
+    delay(500);
+    Serial.print(".");
+    tentativasWiFI++;
+
+  }
+
+  Serial.println();
+  if(WiFi.status() == WL_CONNECTED)
+  {
+    Serial.println("WiFi conectado com sucesso!");
+    Serial.print("Endereço IP: ");
+    Serial.println(WiFi.localIP());
+  }
+
+  else
+  {
+    Serial.println("Falha ao conectar no WiFi.");
+    Serial.println("Verifique SSID, Senha e Sinal de rede ");
+  }
+}
+
+void garantirWifiConectado()
+{
+  if(WiFi.status() != WL_CONNECTED)
+  {
+    Serial.println("WiFi desconectado. Tentando reconectar...");
+    conectarWifi();
+  }
+
+  if(WiFi.status() != WL_CONNECTED)
+  {
+    Serial.println("Não foi possível reconectar ao WiFi.");
+  }
 }
